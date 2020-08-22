dd58c516b8277bd9d29ab86c3fa9f079e8c1d6db
Summer <csy13808796047@gmail.com>
2020-08-22 17:11:47 +0800

    用户界面 - 收藏商品列表

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::redirect('/', '/products')->name('root');
 
 Auth::routes(['verify' => true]);
+Route::get('products', 'ProductsController@index')->name('products.index');
 
 // auth 中间件代表需要登录,verified中间件代表需要经过邮箱验证
 Route::group(['middleware' => ['auth', 'verified']], function () {
@@ -25,9 +26,10 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::get('user_addresses/{user_address}', 'UserAddressesController@edit')->name('user_addresses.edit');
     Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
     Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
-    Route::get('products', 'ProductsController@index')->name('products.index');
-    Route::get('products/{product}', 'ProductsController@show')->name('products.show');
+
     Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
     Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
+    Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
 
 });
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
