9ed6c02b4a2d7d924f9eb8cbf7b265efb81b5fa3
Summer <csy13808796047@gmail.com>
2020-08-29 13:44:23 +0800

    管理后台 - 订单发货

--- app/Admin/Controllers/OrdersController.php
+++ app/Admin/Controllers/OrdersController.php
@@ -2,13 +2,17 @@
 
 namespace App\Admin\Controllers;
 
+use App\Exceptions\InvalidRequestException;
 use App\Models\Order;
 use Encore\Admin\Controllers\AdminController;
 use Encore\Admin\Grid;
 use Encore\Admin\Layout\Content;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
 
 class OrdersController extends AdminController
 {
+    use ValidatesRequests;
 
     protected $title = '订单';
 
@@ -49,5 +53,32 @@ class OrdersController extends AdminController
             ->header('查看订单')
             // body 方法可以接受 Laravel 的视图作为参数
             ->body(view('admin.orders.show', ['order' => Order::find($id)]));
+    }
+    public function ship(Order $order, Request $request)
+    {
+        // 判断当前订单是否支付
+        if (!$order->paid_at) {
+            throw new InvalidRequestException('该订单未付款');
+        }
+        // 判断当前订单发货状态是否为未发货
+        if ($order->ship_status !== Order::SHIP_STATUS_PENDING) {
+            throw new InvalidRequestException('该订单已发货');
+        }
+        $data = $this->validate($request, [
+            'express_company' => ['required'],
+            'express_no' => ['required'],
+        ], [], [
+            'express_company' => '物流公司',
+            'express_no' => '物流单号',
+        ]);
+        // 将订单发货状态改为已发货,并存入物流信息
+        $order->update([
+            'ship_status' => Order::SHIP_STATUS_DELIVERED,
+            // 我们在Order模型的$casts属性里指明了ship_data是一个数组
+            // 因此这里可以把数组传过去
+            'ship_data' => $data,
+        ]);
+        // 返回上一页
+        return redirect()->back();
     }
 }
